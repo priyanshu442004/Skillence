780a1744ac36dd87144840d41aefaf776c4d7261
priyanshu442004 <priyanshusharma442004@gmail.com>
2025-05-09 16:32:58 +0530

    Home page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import Home from './Pages/HomePage/Home'
-import Abc from './Abc'
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/HomePage/Home';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 const App = () => {
   return (
     <div>
+      <Navbar/>
+    <Router>
 
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+    <Footer/>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
