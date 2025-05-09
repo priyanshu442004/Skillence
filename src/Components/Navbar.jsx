d4cef84727ee64811521bdc6af6d2cf6494ca85f
priyanshu442004 <priyanshusharma442004@gmail.com>
2025-05-09 16:51:29 +0530

    Home page design

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -14,7 +14,8 @@ const Navbar = () => {
       <header className="navbar-container">
         <header data-thq="thq-navbar" className="navbar-navbar-interactive">
           <img
-            onClick={window.location.reload}
+            onClick={() => navigate("/")}
+            style={{ cursor: "pointer" }}
             alt="Company Logo"
             src="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/84ec08e8-34e9-42c7-9445-d2806d156403/fac575ac-7a41-484f-b7ac-875042de11f8?org_if_sml=1&amp;force_format=original"
             className="navbar-image1"
